0138d84e27808a66b6d6dca33420b9ab994dc6d6
colesayer <colesayershapiro@gmail.com>
2017-08-30 18:27:29 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require 'pry'
+class School
+
+attr_reader :roster
+
+  def initialize(name)
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster[grade]
+      @roster[grade]<< name
+    else
+      @roster[grade] = []
+      @roster[grade]<< name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster = @roster.sort
+    @roster.map {|k,v| [k,v.sort]}.to_h
+  end
+
+
+
+end
